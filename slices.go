7c856b61367f5a42c3369d1d38d6bf314146f9e8
Liberbell <liberbell@gmail.com>
2020-04-01 10:07:45 +0900

    slices6

--- slices.go
+++ slices.go
@@ -6,7 +6,8 @@ func main() {
 	slice := []int{7, 9, 10, 12}
 	fmt.Println("Slice = ", slice)
 
-	for i := 0; i < len(slice); i++ {
-		fmt.Printf("Slice[%d] = %d\n", i, slice[i])
-	}
+	// for i := 0; i < len(slice); i++ {
+	// 	fmt.Printf("Slice[%d] = %d\n", i, slice[i])
+	// }
+	fmt.Println("Slice[1:4] = ", slice[1:4])
 }
