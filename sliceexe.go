8f3b7337495bfcefdc6486ee9ed164517aaa774b
Liberbell <liberbell@gmail.com>
2020-04-07 08:48:30 +0900

    sliceexe10

--- sliceexe.go
+++ sliceexe.go
@@ -16,5 +16,7 @@ func main() {
 		slice[i] = v - 5
 	}
 	fmt.Println("\nThe new value in our slice:")
-	report("slice", slice)
+	// report("slice", slice)
+
+	fmt.Println("\nNow we`ll append 2 values to slice")
 }
