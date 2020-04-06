228ccdd5d4f12fbbcf68d12de039b99ab25a0f7f
Liberbell <liberbell@gmail.com>
2020-04-06 09:39:38 +0900

    channels4

--- channels.go
+++ channels.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func sum(a []int, ch chan int) {
 	sum := 0
 	for _, v := range a {
@@ -12,4 +14,9 @@ func main() {
 	a := []int{7, 0, -3, 5, 0, 4}
 
 	ch := make(chan int)
+	go sum(a[:len((a)/2)], ch)
+	go sum(a[len(a)/2:], ch)
+	x, y := <-ch, <-ch
+
+	fmt.Println(x, y, x+y)
 }
