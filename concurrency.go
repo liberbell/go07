6114c4b3c7e443836d384d3c26b5669ec868f987
Liberbell <liberbell@gmail.com>
2020-04-06 09:13:04 +0900

    concurrency2

--- concurrency.go
+++ concurrency.go
@@ -20,3 +20,14 @@ func main() {
 	fmt.Println("\nMessage from func main, I`m finished.")
 	time.Sleep(time.Millisecond * 2500)
 }
+
+func msg() {
+	for i := 1; i <= 5; i++ {
+		time.Sleep(time.Millisecond * 1000)
+		if i > 3 {
+			fmt.Println(i, "seconds... yawn")
+		} else {
+			fmt.Println(i, "seconds")
+		}
+	}
+}
